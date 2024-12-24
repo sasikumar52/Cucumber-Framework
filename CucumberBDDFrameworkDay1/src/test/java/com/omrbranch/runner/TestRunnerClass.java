1d6f678cb3fec3c95edc5a1463f9d34a22dec92c
SasiKumar <sasimathiru97@gmail.com>
2024-12-24 18:54:59 +0530

    Cucumber Day 1

---
+++ CucumberBDDFrameworkDay1/src/test/java/com/omrbranch/runner/TestRunnerClass.java
@@ -0,0 +1,12 @@
+package com.omrbranch.runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src\\test\\resources",glue = "com.omrbranch.stepdefinition")
+public class TestRunnerClass {
+
+}
---
+++ CucumberBDDFrameworkDay1/src/test/resources/Features/Login.feature
@@ -0,0 +1,9 @@
+Feature: Verifying OmrBranch Login Module
+
+  Scenario: Verify OmrBranch login with valid credentials
+    Given User is on the OmrBranch Login page
+    When User enters userName and passWord
+    And User clicks Login button
+    Then User should verify after login success message
+
+   
